84b5f65bf23ff359516b34d883000f37a9ee91b9
Ashok Tamang <ashok.tamang@bison.howard.edu>
2017-04-16 21:22:15 -0400

    validate and add Image URL

--- image.js
+++ image.js
@@ -1,10 +1,20 @@
+"use strict";
+
 window.onload = function() {
     $('add').onclick = add;
     $('delete').onclick = remove;
 };
 
 function add() {
-    alert("add");
+    var image = document.createElement("img");
+    image.src = $("url").value;
+    console.log(image.src);
+    image.className = "image";
+    image.onerror = function() {
+        this.remove();
+    };
+    $("blank_area").appendChild(image);
+
 }
 
 function remove() {
